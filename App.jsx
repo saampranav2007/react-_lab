d834d66879bce3eb9bf19d91dc3d5e4e4fcd1ced
SAAMPRANAV T <saampranav27@gmail.com>
2025-12-30 10:46:24 +0530

    30/12/2025

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      
+      <p class="font-bold bg-blue-400 text-white">Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet. </p>
+      <p class="font-bold bg-red-400 text-white" >Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet.</p>
+      <p class="font-bold bg-lime-400 text-white">Lorem ipsum dolor, sit amet consectetur </p>
+      <p class="font-bold bg-pink-400 text-white">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet.</p>
+      <p class="font-bold bg-green-400 text-white">Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+    </div>
+  )
+}
+
+export default App
+
