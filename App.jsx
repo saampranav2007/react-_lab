71f0a59ed3cadee9f6da66d6dd19f089b918bd32
SAAMPRANAV T <saampranav27@gmail.com>
2026-01-03 14:50:23 +0530

    program2

--- App.jsx
+++ App.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+  const [tasks,settask]=useState([]);
+  const [text,settext]=useState("")
+  const addtask=()=>{
+      settask([...tasks ,{name:text, completed:false}])
+      settext("")
+   }
+
+   const deletetask=(index)=>{
+    settask(tasks.filter((_,i)=>i !== index));
+   }
+
   return (
     <div>
-      
-      <p class="font-bold bg-blue-400 text-white">Lorem, ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet. </p>
-      <p class="font-bold bg-red-400 text-white" >Lorem ipsum dolor sit, amet consectetur Lorem ipsum dolor sit amet.</p>
-      <p class="font-bold bg-lime-400 text-white">Lorem ipsum dolor, sit amet consectetur </p>
-      <p class="font-bold bg-pink-400 text-white">Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet.</p>
-      <p class="font-bold bg-green-400 text-white">Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+      <h1>to-do list</h1>
+      <input 
+      value={text}
+      onChange={(e)=>settext(e.target.value)}
+      placeholder='enter task'
+      />
+      <button onClick={addtask}>Add</button>
+
+      <ul>
+        {tasks.map((task,index)=>(
+          <li key={index}>
+            <input
+            type='checkbox'
+            checked={tasks.completed}
+            />
+            <span>{task.name}</span>
+            <button onClick={()=>deletetask(index)}
+            >delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 export default App
-
